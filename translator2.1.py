b8e678b6a194f518cb71a73ca8cd41c1e00a3b22
IssacTsai <issac891015@gmail.com>
2024-07-06 04:13:32 +0800

    v2.1

---
+++ translator2.1.py
@@ -0,0 +1,104 @@
+import UI2
+import sys
+import time
+import keyboard
+import win32clipboard
+
+from PyQt5.QtGui import *
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from googletrans import Translator
+
+tr = Translator()
+language = 'zh-tw'
+
+
+class MyWindow(QtWidgets.QWidget, UI2.Ui_Form):
+
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.t_replace = trans()
+        self.t_replace.start()
+        self.t_replace.signal.connect(self.upgrade)
+
+    def upgrade(self, txt):
+        self.lineEdit.setText(txt)
+
+    def click(self):
+        if self.lineEdit.text() != '':
+            txt_tr = tr.translate(self.lineEdit.text(), dest=language).text
+            win32clipboard.OpenClipboard()
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardData(
+                win32clipboard.CF_UNICODETEXT, str(txt_tr))
+            # win32clipboard.SetClipboardText(str(txt_tr))
+            win32clipboard.CloseClipboard()
+            self.lineEdit.setText(str(txt_tr))
+
+    def zh_tw(self):
+        self.enable()
+        self.pushButton_1.setEnabled(False)
+        global language
+        language = 'zh-tw'
+        self.click()
+
+    def en(self):
+        self.enable()
+        self.pushButton_2.setEnabled(False)
+        global language
+        language = 'en'
+        self.click()
+
+    def jp(self):
+        self.enable()
+        self.pushButton_3.setEnabled(False)
+        global language
+        language = 'ja'
+        self.click()
+
+    def zh_cn(self):
+        self.enable()
+        self.pushButton_4.setEnabled(False)
+        global language
+        language = 'zh-cn'
+        self.click()
+
+    def enable(self):
+        self.pushButton_1.setEnabled(True)
+        self.pushButton_2.setEnabled(True)
+        self.pushButton_3.setEnabled(True)
+        self.pushButton_4.setEnabled(True)
+
+
+class trans(QThread):
+    signal = pyqtSignal(str)
+
+    def __init__(self):
+        super(trans, self).__init__()
+
+    def run(self):
+        data = ''
+        while (1):
+            if keyboard.is_pressed('ctrl') and keyboard.is_pressed('c'):
+                global language
+                time.sleep(0.1)
+
+                # get clipboard data
+                win32clipboard.OpenClipboard()
+                data = win32clipboard.GetClipboardData()
+                txt_tr = tr.translate(data, dest=language).text
+                win32clipboard.EmptyClipboard()
+                win32clipboard.SetClipboardData(
+                    win32clipboard.CF_UNICODETEXT, str(txt_tr))
+                win32clipboard.CloseClipboard()
+                self.signal.emit(txt_tr)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = MyWindow()
+    Form.setWindowTitle("Translator")
+    Form.setWindowFlags(Qt.WindowStaysOnTopHint)
+    Form.show()
+    sys.exit(app.exec_())
